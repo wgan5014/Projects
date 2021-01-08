4859631e8d474a945b115d31b154e1fae70f782e
wgan5014 <73367258+wgan5014@users.noreply.github.com>
2021-01-08 12:33:23 +1100

    Add files via upload

---
+++ RStudio/Machine Learning/CC Fraud Detection/cc_fraud_detection.R
@@ -0,0 +1,44 @@
+library(caret)
+library(ranger)
+library(data.table)
+library(caTools)
+library(pROC)
+library(class)
+
+cc_data <- read.csv("./creditcard.csv") # put dataset here
+cc_data$Amount = scale(cc_data$Amount)
+newData = cc_data[,-c(1)]
+
+# set seed, make training & testing data
+set.seed(123)
+sample = sample.split(newData$Class, SplitRatio = 0.8)
+data_training = subset(newData, sample == TRUE)
+data_test = subset(newData, sample == FALSE)
+
+# logistic regression
+log_mod = glm(Class~., data_test, family = binomial())
+plot(log_mod)
+lr.predict <- predict(log_mod,data_training, probability = TRUE)
+auc.gbm = roc(data_test$Class, lr.predict, plot = TRUE, col = "blue")
+
+# decision tree
+library(rpart)
+library(rpart.plot)
+tree_model <- rpart(Class ~ . , cc_data, method = 'class')
+predicted_val <- predict(tree_model, cc_data, type = 'class')
+probability <- predict(tree_model, cc_data, type = 'prob')
+rpart.plot(tree_model)
+
+# k nearest neighbours
+nn3 <- knn(Class ~ ., data_training, data_test,k=3)
+table(data_test[,"Class"], nn3)
+
+# neural network
+library(neuralnet)
+ANN_model = neuralnet(Class~.,data_training,linear.output=FALSE)
+plot(ANN_model)
+
+predANN=compute(ANN_model,test_data)
+resultANN=predANN$net.result
+resultANN=ifelse(resultANN>0.5,1,0)
+
